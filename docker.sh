93a3ab71be121417147023975514687b81950f43
liyang <53048974+meiweimuli@users.noreply.github.com>
2023-05-13 01:09:29 +0800

    Add files via upload
    docker.sh

---
+++ docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+apt-get -y install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+ufw allow 1:65535/tcp
+
+cat<<EOF > docker-compose.yml
+services:
+  caddyray:
+    image: gczran/caddyray:latest
+    ports:
+      - '80:80'
+      - '443:443'
+      - '10000-10050:10000-10050'
+EOF
+
+docker compose up -d
