60e6c326609207daae2d9279d5a40fde7e35a701
liyang <53048974+meiweimuli@users.noreply.github.com>
2023-06-04 21:59:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get update
-apt-get -y install ca-certificates curl gnupg
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
-  tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 ufw allow 1:65535/tcp
 
-mkdir html
+# 安装docker
+if [ ! -$(command -v docker) ]; then
+  apt-get remove docker docker-engine docker.io containerd runc
+  apt-get update
+  apt-get -y install ca-certificates curl gnupg
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+    tee /etc/apt/sources.list.d/docker.list >/dev/null
+  apt-get update
+  apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+mkdir -p html
+
 cat <<EOF >html/index.html
 <!DOCTYPE html>
 <html lang="en">
@@ -167,4 +171,5 @@ networks:
     driver: bridge
 EOF
 
+docker compose down
 docker compose up -d
